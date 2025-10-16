402f78f6c3cb3be7187a2da24b2c18a929369a56
v138468 <v138468@gmail.com>
2025-10-16 10:06:57 +0900

    把匹配改成巩固, 学习学过的单词

--- scripts/models/japanese_model.gd
+++ scripts/models/japanese_model.gd
@@ -1,8 +1,13 @@
 extends DataModelBase
 class_name JapaneseModel
 
+var words_group_by_book = {}
+
 func _ready() -> void:
-	pass
+	for book in GlobalDb.words.books.values():
+		words_group_by_book[book.id] = []
+	for word in GlobalDb.words.japanese.values():
+		words_group_by_book[word.book_id].append(word)
 
 func get_words_of_book(book_id: int, need_shuffle: bool = true):
 	var card_ids = []
@@ -14,3 +19,14 @@ func get_words_of_book(book_id: int, need_shuffle: bool = true):
 
 func get_word_belong_book(word_id):
 	return GlobalDb.words.japanese[word_id].book_id
+
+func get_for_match_use_words_of_book(book_id):
+	var words = words_group_by_book[book_id]
+	words.shuffle()
+	var know = GlobalStorage.get_value("right", {}) as Dictionary
+	var result = []
+	for word in words:
+		if know.has(str(word.id)):
+			result.append(word.id)
+	print(result)
+	return result
--- scripts/views/main_menu.gd
+++ scripts/views/main_menu.gd
@@ -15,7 +15,11 @@ func _on_button_learn_pressed() -> void:
 	
 
 func _on_button_match_game_pressed() -> void:
-	_open_view_wait_book(Const.Views.match_game)
+	var select_book = SceneManager.open_view(Const.Views.select_book, false)
+	select_book.set_info(book_ids, func(book):
+		select_book.queue_free()
+		SceneManager.open_view(Const.Views.match_game).init(Model.JapaneseModel.get_for_match_use_words_of_book(book.id))
+	)
 	
 
 func _on_button_exam_pressed() -> void:
