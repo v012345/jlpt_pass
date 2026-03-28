95a28d03275dcf6241f61d6d72b43b7725dbbe6b
v012345 <v012345@163.com>
2026-03-28 09:18:11 +0800

    1

--- scripts/global/global_db.gd
+++ scripts/global/global_db.gd
@@ -11,6 +11,7 @@ func _ready() -> void:
 	var file = FileAccess.open("user://HeroList.json", FileAccess.READ)
 	if leaper_tables["HeroList"] == null:
 		print("文件打开失败:", file)
+		return
 	# # 2. 读取文本
 	var text = file.get_as_text()
 	# # 3. 解析 JSON
@@ -41,4 +42,3 @@ func save_leaper_table(table_name: String, data):
 	var json = JSON.stringify(data)
 	file.store_string(json)
 	file.close()
-
